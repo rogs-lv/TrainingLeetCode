9e9bb57b9f3723f7142cedbf287670315a93f226
Oliver Rosas Gonzalez <oliverrosasg@gmail.com>
2021-02-17 15:37:18 -0600

    First commit

---
+++ 1470.ShuffletheArray/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _1470.ShuffletheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            int n = rd.Next(2, 10);
+            //Only pair numbers
+            /*while (n%2 != 0) {
+                n = rd.Next(2, 20);
+            }*/
+            int len = n * 2;
+            int[] arr = new int[len];
+            for (int j = 0; j < len; j++) {
+                arr[j] = rd.Next(2, 100);
+            }
+            Console.WriteLine("Input: n: {0}, nums: [{1}]", n, String.Join(",",arr));
+            int[] result = Shuffle(arr, n);
+            Console.WriteLine("Output: {0}", String.Join(",", result));
+        }
+        public static int[] Shuffle(int[] nums, int n)
+        {
+            int lenArray = nums.Length;
+            int[] newArr = new int[lenArray];
+            bool flag = true;
+            int j = n;
+            int l = 0;
+            for (int i = 0; i < lenArray; i++)
+            {
+                if (flag)
+                {
+                    newArr[i] = nums[l];
+                    l++;
+                    flag = false;
+                }
+                else
+                {
+                    newArr[i] = nums[j];
+                    j++;
+                    flag = true;
+                }
+            }
+            return newArr;
+        }
+    }
+}
---
+++ 1480.RunningSumof1dArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _1480.RunningSumof1dArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            int size = rd.Next(1,10);
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = rd.Next(1, 50);
+            }
+            Console.WriteLine("Input : " +  String.Join(",", arr));
+            int[] result = RunningSum(arr);
+            Console.WriteLine("Output : " + String.Join(",", result));
+        }
+        public static int[] RunningSum(int[] nums)
+        {
+            int len = nums.Length;
+            int[] newArray = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                if (i > 0)
+                {
+                    int acum = 0;
+                    for (int j = 0; j <= i; j++)
+                    {
+                        acum += nums[j];
+                        newArray[i] = acum;
+                    }
+                }
+                else
+                    newArray[i] = nums[i];
+            }
+            return newArray;
+        }
+    }
+}
